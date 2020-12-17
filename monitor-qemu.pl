e06b15b9e2fa745a04b972c446093e1d8faefff3
Ben <benrude@mailbox.org>
2020-12-17 15:28:35 -0800

    updates

--- monitor-qemu.pl
+++ monitor-qemu.pl
@@ -1,21 +1,30 @@
 #!/usr/bin/perl -w
 use strict;
 
-my $running = `ps -A -ef | grep win10 | grep qemu | grep -v "ps -A -ef" | wc -l`;
+
+my $running = `ps -A -ef | grep Murmur | grep qemu | grep -v "ps -A -ef" | wc -l`;
 chomp($running);
 my $checkval = 0;
 
+print 'running: ' . $running . "\n";
+print 'checkval: ' . $checkval . "\n";
+
+
 while(1){
-    $checkval = `ps -A -ef | grep win10 | grep qemu | grep -v "ps -A -ef" | wc -l`;
+    $checkval = `ps -A -ef | grep Murmur | grep qemu | grep -v "ps -A -ef" | wc -l`;
     chomp($checkval);
     
     if($checkval && !$running){
         $running = 1;
-        system("monitor-set --off");
+        system("~/scripts/monitor-set --off");
     } elsif(!$checkval && $running) {
         $running = 0;
-        system("monitor-set --on");
+        system("~/scripts/monitor-set --on");
     }
+
+   print 'running: ' . $running . "\n";
+   print 'checkval: ' . $checkval . "\n";
 
     sleep(8);
 }
+
